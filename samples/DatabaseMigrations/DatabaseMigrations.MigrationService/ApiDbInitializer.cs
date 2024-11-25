372d12ffb4aaa9e5f1f80974336d3d7659a3dd18
274188A <40014477+274188A@users.noreply.github.com>
2024-11-25 11:34:54 +0800

    dotnet9.0

--- samples/DatabaseMigrations/DatabaseMigrations.MigrationService/ApiDbInitializer.cs
+++ samples/DatabaseMigrations/DatabaseMigrations.MigrationService/ApiDbInitializer.cs
@@ -1,14 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.ComponentModel;
 using System.Diagnostics;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.Extensions.Logging;
-using OpenTelemetry.Trace;
 
 namespace DatabaseMigrations.MigrationService;
 
@@ -58,13 +54,6 @@ public class ApiDbInitializer(
 
     private static async Task RunMigrationAsync(MyDb1Context dbContext, CancellationToken cancellationToken)
     {
-        var strategy = dbContext.Database.CreateExecutionStrategy();
-        await strategy.ExecuteAsync(async () =>
-        {
-            // Run migration in a transaction to avoid partial migration if it fails.
-            await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
-            await dbContext.Database.MigrateAsync(cancellationToken);
-            await transaction.CommitAsync(cancellationToken);
-        });
+        await dbContext.Database.MigrateAsync(cancellationToken);
     }
 }
